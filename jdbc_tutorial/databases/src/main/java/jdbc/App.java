0eebdcaf602d3bddc9ad6cee0142a2a3dd3e61cf
Juan Carlos Hernandez Iñiguez <juanhernandez@Juans-MacBook-Pro-2.local>
2018-12-03 12:58:11 -0600

    executing a static SQL

--- jdbc_tutorial/databases/src/main/java/jdbc/App.java
+++ jdbc_tutorial/databases/src/main/java/jdbc/App.java
@@ -1,8 +1,6 @@
 package jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * Hello world!
@@ -21,10 +19,15 @@ public class App
     public static void main( String[] args ) throws SQLException
     {
         Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
 
         try {
             conn = DriverManager.getConnection(CONN_STRING, USER, PASSWORD);
-            System.out.println("Connected!");
+            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            rs = stmt.executeQuery("SELECT * FROM STATES");
+            rs.last();
+            System.out.println("number of rows!: " + rs.getRow());
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
