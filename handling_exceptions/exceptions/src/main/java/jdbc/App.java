93d73af93610323df128ebb67065e98bf69c41f4
Juan Carlos Hernandez Iñiguez <juanhernandez@Juans-MacBook-Pro-2.local>
2018-12-04 10:30:39 -0600

    try with  resources

--- handling_exceptions/exceptions/src/main/java/jdbc/App.java
+++ handling_exceptions/exceptions/src/main/java/jdbc/App.java
@@ -1,6 +1,9 @@
 package jdbc;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Hello world!
@@ -10,31 +13,17 @@ public class App
 {
     public static void main( String[] args ) throws SQLException
     {
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
 
-        try {
-            conn = DBUtil.getConnection(DBType.MYSQL);
-            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            rs = stmt.executeQuery("SELECT stateId, stateName FROM s");
+        try (
+                Connection conn = DBUtil.getConnection(DBType.MYSQL);
+                Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                ResultSet rs = stmt.executeQuery("SELECT stateId, stateName FROM s")
+                ){
             rs.last();
-
             System.out.println("number of rows: " + rs.getRow());
         } catch (SQLException e) {
             DBUtil.processException(e);
-        } finally {
-            if(rs != null){
-                rs.close();
-            }
-
-            if(stmt != null){
-                stmt.close();
-            }
-
-            if(conn != null){
-                conn.close();
-            }
         }
+
     }
 }
